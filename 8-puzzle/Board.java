3ab196c6e752864f253eb8c4a9a3ccab79ce44db
jerome <jerometagubanoone@gmail.com>
2022-04-27 21:45:16 +0800

    Migrated to new laptop lost the last solver

--- 8-puzzle/Board.java
+++ 8-puzzle/Board.java
@@ -1,61 +1,45 @@
 import edu.princeton.cs.algs4.Stack;
 
-public class Board {
+import java.awt.*;
 
+public class Board {
     private final int[][] board;
     private int hammingCount;
     private int manhattanCount;
     private int emptyI;
     private int emptyJ;
     private final StringBuilder boardString;
-    
     // @param 2 <= n <= 128
     public Board(int[][] tiles) {
-        
         if (tiles == null) throw new IllegalArgumentException("Board can't be null");
         board = new int[tiles.length][tiles.length];
-        
         hammingCount = 0;
         manhattanCount = 0;
-        
         boardString = new StringBuilder("" + this.dimension() + "\n ");
-        
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 board[i][j] = tiles[i][j];
-                boardString.append(board[i][j] + "  ");
+                boardString.append(board[i][j]);
+                boardString.append("  ");
             }
             boardString.append("\n ");
         }
-        
         calculateDistances();
-        
     }
-    
     public String toString() {
-        
         return boardString.toString();
     }
-    
     public int dimension() {
-        
         return board.length;
     }
-    
     public int hamming() {
-        
         return hammingCount;
     }
-    
     public int manhattan() {
-        
         return manhattanCount;
     }
-    
     private void calculateDistances() {
-        
         int tempCounter = 0;
-        
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 tempCounter++;
@@ -65,7 +49,6 @@ public class Board {
                     emptyJ = j;
                    continue;
                 }
-                
                 if (board[i][j] != tempCounter) {
                     hammingCount++;
                     manhattanCount += calculateManhattan(i, j, board[i][j]);
@@ -73,12 +56,9 @@ public class Board {
             }
         }
     }
-    
     private int calculateManhattan(int currentI, int currentJ, int value) {
-        
         int correctI = 0;
         int correctJ = 0;
-
         if (value % board.length == 0) {
             // since array starts at 0
             correctI = (value / board.length) - 1;
@@ -86,37 +66,24 @@ public class Board {
         } else {
             correctI = (value / board.length);
             correctJ = (value % board.length) - 1;
-            
         }
-        
         return Math.abs(currentI - correctI) + Math.abs(currentJ - correctJ);
     }
-    
     public boolean isGoal() {
-        
         return manhattanCount == 0;
     }
-    
     public boolean equals(Object y) {
-        
         if (y == null) return false;
-        if (y == this) return true;
         if (y.getClass() != this.getClass()) return false;
-        
         return this.toString().equals(y.toString());
     }
-    
     public Iterable<Board> neighbors() {
-        
         Stack<Board> stackBoard = new Stack<Board>();
-        
         // this will set true for corners that is out of bounds/edges
         boolean addedTop = emptyI == 0;
         boolean addedBottom = emptyI == board.length - 1;
         boolean addedLeft = emptyJ == 0;
         boolean addedRight = emptyJ == board.length - 1;
-        
-        
         if (!addedTop) {
             Board topNeighbor = getNeighborBoard(1);
             stackBoard.push(topNeighbor);
@@ -133,14 +100,11 @@ public class Board {
             Board rightNeighbor = getNeighborBoard(4);
             stackBoard.push(rightNeighbor);
         }
-        
         return stackBoard;
     }
-    
     // code 1 = top, 2 = bottom, 3 = left, 4 = right;
     private Board getNeighborBoard(int pos) {
         int[][] tempBoard = copyBoard();
-        
         if (pos == 1) {
             tempBoard[emptyI][emptyJ] = tempBoard[emptyI - 1][emptyJ];
             tempBoard[emptyI - 1][emptyJ] = 0;
@@ -154,17 +118,13 @@ public class Board {
             tempBoard[emptyI][emptyJ] = tempBoard[emptyI][emptyJ + 1];
             tempBoard[emptyI][emptyJ + 1] = 0;
         }
-        
         return new Board(tempBoard);
     }
-    
     public Board twin() {
-        
         int rowA = 0;
         int colA = 0;
         int rowB = 0;
         int colB = 0;
-        
         if (rowA == emptyI && colA == emptyJ) {
             if (rowA < board.length - 1) rowA++;
             else colA++;
@@ -173,21 +133,14 @@ public class Board {
             if (rowB < board.length - 1) rowB++;
             else colB++;
         }
-        
-        
         int[][] tempBoard = copyBoard();
-        
         int temp = tempBoard[rowA][colA];
         tempBoard[rowA][colA] = tempBoard[rowB][colB];
         tempBoard[rowB][colB] = temp;
-        
         return new Board(tempBoard);
     }
-    
     private int[][] copyBoard() {
-        
         int[][] tempBoard = new int[board.length][board.length];
-        
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
                 tempBoard[i][j] = board[i][j];
@@ -195,18 +148,12 @@ public class Board {
         }
         return tempBoard;
     }
-    
     public static void main(String[] args) {
-        
         int[][] test = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
         Board testBoard = new Board(test);
         System.out.println(testBoard.toString());
-        
         int[][] test2 = {{1, 2, 3}, {4, 8, 6}, {7, 5, 0}};
-        
         Board testBoard2 = new Board(test2);
-        
         System.out.println(testBoard.equals(testBoard2));
     }
-    
 }
--- 8-puzzle/Solver.java
+++ 8-puzzle/Solver.java
@@ -1,33 +1,24 @@
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.MinPQ;
-
 public class Solver {
-    
     private Board board;
-    
     public Solver(Board initial) {
         // update this
         board = initial;
         MinPQ<Board> priorityQueue = new MinPQ<Board>();
-        
         // just to satisfy the error
         initial.manhattan();
     }
-    
     public boolean isSolvable() {
         return false;
     }
-    
     public int moves() {
         return 0;
     }
-    
     public Iterable<Board> solution() {
         return null;
     }
-    
     public static void main(String[] args) {
-        
         // create initial board from file
         In in = new In(args[0]);
         int n = in.readInt();
@@ -36,10 +27,8 @@ public class Solver {
             for (int j = 0; j < n; j++)
                 tiles[i][j] = in.readInt();
         Board initial = new Board(tiles);
-
         // solve the puzzle
         Solver solver = new Solver(initial);
-
         // print solution to standard output
         if (!solver.isSolvable())
             System.out.println("No solution possible");
@@ -48,7 +37,5 @@ public class Solver {
             for (Board board : solver.solution())
                 System.out.println(board);
         }
-        
     }
-
 }
